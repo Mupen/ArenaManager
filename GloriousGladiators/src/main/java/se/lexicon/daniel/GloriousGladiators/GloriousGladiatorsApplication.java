0067d3a35f780dad1815b4d42b83d32b56d8a886
Mupen <45752236+Mupen@users.noreply.github.com>
2019-04-03 15:46:12 +0200

    start

--- GloriousGladiators/src/main/java/se/lexicon/daniel/GloriousGladiators/GloriousGladiatorsApplication.java
+++ GloriousGladiators/src/main/java/se/lexicon/daniel/GloriousGladiators/GloriousGladiatorsApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GloriousGladiatorsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GloriousGladiatorsApplication.class, args);
+	public static void main(String[] args) { 
+		SpringApplication.run(GloriousGladiatorsApplication.class, args); 
+		
 	}
-
 }
