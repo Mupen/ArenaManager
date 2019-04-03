a24a93c3ad1cd2a1d1e10d9e848ae3f7d29fd9b9
Mupen <45752236+Mupen@users.noreply.github.com>
2019-04-03 15:09:31 +0200

    my project start

---
+++ GloriousGladiators/src/main/java/se/lexicon/daniel/GloriousGladiators/GloriousGladiatorsApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.daniel.GloriousGladiators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloriousGladiatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloriousGladiatorsApplication.class, args);
+	}
+
+}
